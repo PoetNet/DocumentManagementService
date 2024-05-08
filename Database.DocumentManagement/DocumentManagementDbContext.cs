e31c44de1aa15cc6cac78b033d5ecc1732575c9a
PoetNet <marfutin.00@mail.ru>
2024-05-08 16:51:57 +0300

    feat: Update README and DbContext

--- Database.DocumentManagement/DocumentManagementDbContext.cs
+++ Database.DocumentManagement/DocumentManagementDbContext.cs
@@ -6,19 +6,11 @@ namespace Database.DocumentManagement;
 public class DocumentManagementDbContext : DbContext
 {
     public DocumentManagementDbContext(DbContextOptions<DocumentManagementDbContext> options) : base(options)
-    {
-        //If we don't need to use migrations:
-        //Database.EnsureCreated();
-    }
+    { }
 
     public DbSet<Document> Documents => Set<Document>();
     public DbSet<TaskItem> TaskItems => Set<TaskItem>();
-
-    protected override void OnModelCreating(ModelBuilder modelBuilder)
-    {
-        //modelBuilder.ApplyConfigurationsFromAssembly(GetType().Assembly);
-    }
-
+    
     protected override void OnConfiguring(DbContextOptionsBuilder options)
     {
         options.UseSqlite(@"Data Source=..\DocumentManagementDb.db",
